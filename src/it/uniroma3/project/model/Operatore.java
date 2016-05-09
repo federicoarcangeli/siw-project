1ab5a04a5126f59313fdddd0e16f0d453353908a
1federico1 <federico.arcangeli1@gmail.com>
2016-05-09 22:21:08 +0200

    ennesimo commit

--- src/it/uniroma3/project/model/Operatore.java
+++ src/it/uniroma3/project/model/Operatore.java
@@ -1,5 +1,6 @@
 package it.uniroma3.project.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -30,6 +31,88 @@ public class Operatore {
 	private List<Ordine> orders;
 
 	public Operatore() {
+		this.orders = new ArrayList<>();
 	}
+	
+	public void addOrder(Ordine o) {
+		this.orders.add(o);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCodice() {
+		return codice;
+	}
+
+	public void setCodice(String codice) {
+		this.codice = codice;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Ordine> getOrders() {
+		return orders;
+	}
+
+	public void setOrders(List<Ordine> orders) {
+		this.orders = orders;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codice == null) ? 0 : codice.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((orders == null) ? 0 : orders.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Operatore other = (Operatore) obj;
+		if (codice == null) {
+			if (other.codice != null)
+				return false;
+		} else if (!codice.equals(other.codice))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (orders == null) {
+			if (other.orders != null)
+				return false;
+		} else if (!orders.equals(other.orders))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		return true;
+	}
+	
+	
 
 }
