f9b44904d0aac4eedc64c9a1dd6591b00f438847
AlessandroGiannetti <ale.giannetti@hotmail.it>
2016-06-15 15:32:29 +0200

    modificata strategia fetch linea comanda su comanda Lazy

--- src/it/uniroma3/project/model/LineaComanda.java
+++ src/it/uniroma3/project/model/LineaComanda.java
@@ -2,6 +2,7 @@ package it.uniroma3.project.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -20,7 +21,7 @@ public class LineaComanda {
 	@Column(nullable = false)
 	private int quantita;
 
-	@ManyToOne
+	@ManyToOne(fetch=FetchType.LAZY)
 	private Comanda comanda;
 
 	@ManyToOne
